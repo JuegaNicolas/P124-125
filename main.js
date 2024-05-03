b32a32eafb8ce64e86fddb9f8d5832b5ed1918b9
JuegaNicolas <149122964+JuegaNicolas@users.noreply.github.com>
2024-05-03 15:22:22 -0500

    ÑAM
    ÑAM

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+    clown_nose = loadImage('https://postimg.cc/2bFghxzT');
+    }
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+    }
+    
+
+
+    function draw() {
+        image(video, 0, 0, 300, 300);
+        fill(16, 227, 216);
+        stroke(204, 24, 183);
+        circle(noseX, noseY, 20);
+        image(clown_nose, noseX, noseY, 30, 30);
+    }
+    
+function modelLoaded () {
+    console.log('PoseNet está inicializado');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0) {
+    
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("nose x =" + noseX);
+    console.log("nose y =" + noseY);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+}
